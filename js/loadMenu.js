f347f572825c84a54d08ef47610efd4b35424571
alejandro91dc <diazcarmona.alejandro91@gmail.com>
2023-11-16 02:51:26 +0100

    menu global
    para contact, gallery e integracion de todas las paginas

---
+++ js/loadMenu.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuContainer = document.getElementById('headerContainer'); 
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            menuContainer.innerHTML = this.responseText;
+            menuContainer.style.display = 'block'; // Mostrar el contenedor
+   
+
+        }
+    };
+    xhttp.open("GET", "../menu.html", true);
+    xhttp.send();
+});
