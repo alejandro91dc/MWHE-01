c53c27e85da4979588e0cca112a72b9d7405ad8b
alejandro91dc <diazcarmona.alejandro91@gmail.com>
2023-11-16 22:48:26 +0100

    footer added
    footer añadido hero modificado con imagen nuestra

--- js/loadMenu.js
+++ js/loadMenu.js
@@ -12,3 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
     xhttp.open("GET", "../menu.html", true);
     xhttp.send();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuContainer = document.getElementById('footerContainer'); 
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            menuContainer.innerHTML = this.responseText;
+            menuContainer.style.display = 'block'; // Mostrar el contenedor
+   
+
+        }
+    };
+    xhttp.open("GET", "../footer.html", true);
+    xhttp.send();
+});
+
